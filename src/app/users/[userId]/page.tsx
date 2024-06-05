a41ee6d674a49db2411105bc3e781cfd4b49cbc5
Ger2k <musikadicto@gmail.com>
2024-06-05 10:35:47 +0200

    boton para volver atrás en userId

--- src/app/users/[userId]/page.tsx
+++ src/app/users/[userId]/page.tsx
@@ -1,11 +1,24 @@
+"use client"
+import { useRouter } from 'next/navigation';
+import { Button } from "@/components/ui/button";
+
 export default function UserDetails({ params } : {
     params: {
         userId: string;
     }
+}) {
+    const router = useRouter();
 
-}){
-    {console.log(params)}
+    const handleBackClick = () => {
+        router.push('/users');
+    };
+
     return (
-        <h1>Id del usuario: {params.userId}</h1>
-    )
+        <div>
+            <h1 className="m-4 px-4 py-2">Id del usuario: {params.userId}</h1>
+            <Button variant="default" onClick={handleBackClick} className="m-4 px-4 py-2">
+                Volver a Usuarios
+            </Button>
+        </div>
+    );
 }
